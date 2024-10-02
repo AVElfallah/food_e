a8f74e63ab7635c853383c939c7b154c096b352a
AVElfallah <abdohegag@gmail.com>
2024-10-02 17:02:54 +0300

    contact support page create

--- lib/pages/profile/pages/contact_support/contact_support_page.dart
+++ lib/pages/profile/pages/contact_support/contact_support_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_e/helpers/colors_helper.dart';
+import 'package:food_e/shared/widgets/role_model_appbar_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ContactSupportPage extends StatelessWidget {
   const ContactSupportPage({super.key});
@@ -6,16 +9,87 @@ class ContactSupportPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ContactSupportPage'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ContactSupportPage is working',
-          style: TextStyle(fontSize: 20),
+        appBar: RoleModelAppbarWidget(
+          context,
+          leadingIcon: Icons.arrow_back_ios_new,
         ),
-      ),
-    );
+        body: Stack(
+          children: [
+            //Top text [START]
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 18,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'CONTACT SUPPORT',
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 36,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.phone_outlined,
+                        color: ColorsHelper.primary,
+                      ),
+                      onTap: () {
+                        //TODO - add acction to phone number
+                      },
+                      title: Text(
+                        '+00 123 456 7890',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.email_outlined,
+                        color: ColorsHelper.primary,
+                      ),
+                      onTap: () {
+                        //TODO - add acction to gmail
+                      },
+                      title: Text(
+                        'support@foode.com',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.chat_outlined,
+                        color: ColorsHelper.primary,
+                      ),
+                      onTap: () {
+                        //TODO - add acction to chat
+                      },
+                      title: Text(
+                        'chat on WhatsApp',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            //Top Text [END]
+          ],
+        ));
   }
 }
